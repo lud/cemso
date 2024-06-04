5b36046dcccdc23ba15d7d3a49149feb5ba025af
lud <ludovic@demblans.com>
2024-06-04 18:41:17 +0200

    file buffer incoming

---
+++ lib/cemso/convert_vec/file_buffer.ex
@@ -0,0 +1,30 @@
+defmodule Cemso.ConvertVec.Buffer do
+  defstruct handle: nil, local: <<>>
+
+  def open(path) do
+    with {:ok, handle} <- File.open(path, [:read, :binary]) do
+      {:ok, %__MODULE__{handle: handle, local: <<>>}}
+    end
+  end
+
+  def load(%{handle: handle, local: local} = buf, n_bytes) do
+    local_size = byte_size(local)
+
+    case max(0, n_bytes - local_size) do
+      0 ->
+        buf
+
+      n ->
+        case IO.read(handle, n) do
+          :eof -> buf
+          data -> %{buf | local: <<local::binary, data::binary>>}
+        end
+    end
+  end
+
+  def consume(buf, preload, consumer) do
+    buf = load(buf, preload)
+    {return_val, remaining_local} = consumer.(buf.local)
+    {return_val, %{buf | local: remaining_local}}
+  end
+end
---
+++ test/buffer_test.exs
@@ -0,0 +1,41 @@
+defmodule Cemso.ConvertVec.BufferTest do
+  alias Cemso.ConvertVec.Buffer
+  use ExUnit.Case, async: true
+
+  test "can read from a file" do
+    file_contents = <<1, 2, 3, 4, 5, 6, 7, 8, 9, 10>>
+    path = Briefly.create!()
+    File.write!(path, file_contents)
+    assert {:ok, buf} = Buffer.open(path) |> dbg()
+    assert <<>> == buf.local
+  end
+
+  defp buffer_from_bin(binary) do
+    file_contents = binary
+    path = Briefly.create!()
+    File.write!(path, file_contents)
+    assert {:ok, buf} = Buffer.open(path)
+    buf
+  end
+
+  test "ask for more than possible" do
+    buf = buffer_from_bin(<<1, 2, 3>>)
+    assert %{local: <<1, 2, 3>>} = Buffer.load(buf, 1_000_000)
+  end
+
+  test "consume with preload" do
+    buf = buffer_from_bin("9999" <> <<1, 2, 3, 4>>)
+    assert <<>> = buf.local
+
+    assert {:some_returned, %{local: <<1>>}} =
+             Buffer.consume(buf, 5, fn "9999" <> rest ->
+               # we return a value and the last byte asked (we matched on 4 but asked 5)
+               {:some_returned, rest}
+             end)
+
+    assert {:retval, %{local: <<2, 3, 4>>}} =
+             Buffer.consume(buf, 10000, fn untouched ->
+               {:retval, untouched}
+             end)
+  end
+end
